75f64e4e2e4d8cfab02097702bff299f9997ed04
Lionel Floriani <lionel.floriani@outlook.com>
2024-09-20 15:52:29 +0200

    Added custom variables for a cohesive and consistent design.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,41 @@ module.exports = {
             fontSize: {},
             borderRadius: {},
             screens: {},
+            spacing: {
+                1: '0.25rem',
+                2: '0.50rem',
+                3: '0.75rem',
+                4: '1rem',
+                5: '1.25rem',
+                6: '1.75rem',
+                7: '2.5rem',
+                8: '3.75rem',
+                9: '6.25rem',
+                10: '10rem',
+                11: '15rem',
+            },
         },
     },
-    plugins: [],
+    // in order to unlock IntelliSens for the custom classes, they have to be declared in `./src/styles/custom.css` and to be registered bellow.
+    plugins: [
+        function ({ addComponents }) {
+            addComponents({
+                '.h1': {},
+                '.h2': {},
+                '.h3': {},
+                '.h4': {},
+                '.big': {},
+                '.p': {},
+                '.small': {},
+                '.dark-color': {},
+                '.light-color': {},
+                '.primary-color': {},
+                '.secondary-color': {},
+                '.primary-button': {},
+                '.secondary-button': {},
+                '.primary-link': {},
+                '.secondary-link': {},
+            });
+        },
+    ],
 };
